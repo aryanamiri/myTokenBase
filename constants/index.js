82eb9d458c808da2832ba222a98efd764f154245
aryanamiri <95675036+aryanamiri@users.noreply.github.com>
2023-08-01 19:46:44 +0330

    Update index.js
    Update Contract

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0xd48d3c3aa6bf37aabe0c60f186addcf84b414e69";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
